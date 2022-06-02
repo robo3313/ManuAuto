79689ef5b3da079ff277f6994b8449e42907682f
ilias12548 <ildd92@outlook.com>
2022-06-02 16:10:15 +0200

    Recreation de la solution du projet

---
+++ La Manu Auto/LaManuAuto/LaManuAuto/Areas/Identity/Data/LaManuAutoContext.cs
@@ -0,0 +1,34 @@
+﻿using LaManuAuto.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LaManuAuto.Data;
+
+public class LaManuAutoContext : IdentityDbContext<LaManuAutoUser>
+{
+    public LaManuAutoContext(DbContextOptions<LaManuAutoContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+
+    }
+
+    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<LaManuAutoUser>
+    {
+        public void Configure(EntityTypeBuilder<LaManuAutoUser> builder)
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(255);
+            builder.Property(u => u.LastName).HasMaxLength(255);
+        }
+    }
+}
