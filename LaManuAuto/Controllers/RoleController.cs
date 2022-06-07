2c4ce09b57303f5ef49c4db5394bf000824012eb
ilias12548 <ildd92@outlook.com>
2022-06-07 10:21:44 +0200

    attribution des roles a la page gestion des utilisateurs

--- LaManuAuto/Controllers/RoleController.cs
+++ LaManuAuto/Controllers/RoleController.cs
@@ -5,19 +5,19 @@ namespace LaManuAuto.Controllers
 {
     public class RoleController : Controller
     {
+        [Authorize(Policy = "RequireAdmin")]
         public IActionResult Index()
         {
             return View();
         }
 
-        [Authorize(Policy = "RequireManager")]
+        [Authorize(Policy = "RequireAdmin")]
         public IActionResult Manager()
         {
             return View();
         }
 
-        //[Authorize(Policy = "RequireAdmin,RequireManager")]
-        [Authorize(Roles = "RequireAdmin,RequireManager")]
+        [Authorize(Policy = "RequireAdmin")]
         public IActionResult Admin()
         {
             return View();
