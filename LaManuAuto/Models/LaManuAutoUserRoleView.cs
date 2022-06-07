667a25c5dc057658bde572830203615b15a8590c
Romain <late2201@gmail.com>
2022-06-07 15:24:46 +0200

    Implemeted display of users

---
+++ LaManuAuto/Models/LaManuAutoUserRoleView.cs
@@ -0,0 +1,9 @@
+﻿using LaManuAuto.Areas.Identity.Data;
+
+namespace LaManuAuto.Models;
+
+public class LaManuAutoUserRoleView
+{
+   public LaManuAutoUser User { get; set; }
+    public IEnumerable<string> Roles { get; set; }
+}
